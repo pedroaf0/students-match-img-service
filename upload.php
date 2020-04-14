8dab205a6ef857252b3c2929f5430cb4ae6c75c0
pedroaf0 <pedroaf1233@gmail.com>
2020-04-14 12:00:08 -0300

    first commit

---
+++ upload.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+echo $_GET['id'];
+$target_dir = "img/";
+$target_file = $target_dir . $_GET['id'];
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    } else {
+    }
+}
+?>
